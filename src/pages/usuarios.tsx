8040a955d7635aa8df0ae71d329fd49e8932273e
DeveloperStack2022 <jlzyjose@gmail.com>
2022-08-18 00:45:55 -0500

    update form of users

--- src/pages/usuarios.tsx
+++ src/pages/usuarios.tsx
@@ -1,15 +1,42 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Stack, Form, Button } from "react-bootstrap";
+
 //componenst
 import FormUsers from "../components/FormUsuarios";
 import TableUsers from "../components/TableUser";
+
 export default function Usuarios() {
+  const [State, setState] = useState<number>(0);
+  const [Change, setChange] = useState<Boolean>(false);
+  const handleCheck = (val: number) => {
+    setState(val);
+    setChange(true);
+  };
   return (
     <>
       <h2>Usuarios</h2>
       <Container>
         <Row>
           <Col className="p-4 m-auto shadow-sm rounded-lg">
+            <Stack gap={2} className="col-md-5 mx-auto">
+              <Button
+                variant={State == 0 ? "primary" : "outline-primary"}
+                onClick={() => handleCheck(0)}
+              >
+                Estudiante
+              </Button>
+              <Button
+                variant={State == 1 ? "primary" : "outline-primary"}
+                onClick={() => handleCheck(1)}
+              >
+                Docente
+              </Button>
+              {!Change && (
+                <p className="h6 text-center text-muted">
+                  Por defecto esta en Estudiante
+                </p>
+              )}
+            </Stack>
             <FormUsers />
           </Col>
           <Col>
